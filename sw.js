19a2f4638f69700b50aae2e80e372ea8f1bfb4ff
1187-ar7 <athulrk2011@gmail.com>
2026-05-23 18:54:23 +0530

    Phase 3 update

--- sw.js
+++ sw.js
@@ -1,9 +1,34 @@
-self.addEventListener("install",(e)=>{
+const CACHE_NAME = "ar7-cache-v1";
 
-self.skipWaiting();
+const urlsToCache = [
+  "./",
+  "./index.html"
+];
+
+self.addEventListener("install",(event)=>{
+
+  event.waitUntil(
+
+    caches.open(CACHE_NAME)
+    .then((cache)=>{
+      return cache.addAll(urlsToCache);
+    })
+
+  );
 
 });
 
-self.addEventListener("fetch",(e)=>{
+self.addEventListener("fetch",(event)=>{
+
+  event.respondWith(
+
+    caches.match(event.request)
+    .then((response)=>{
+
+      return response || fetch(event.request);
+
+    })
+
+  );
 
 });
